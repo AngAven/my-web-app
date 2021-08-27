570dee6da4c7159f7f0b728d4a8003f7abac8b40
Angel <angel.aven.c@gmail.com>
2021-08-26 23:30:10 -0500

    White body

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
-// Create a condition that targets viewports at least 768px wide
+
+
+// Create a condition that targets viewports at least 600px wide
 const mobile = window.matchMedia('(max-width: 600px)')
 
 function handleTabletChange(e) {
   if (e.matches) {
-    console.log('Media Query Matched! 600px')
+    // console.log('Media Query Matched! 600px')
   }
 }
 
-// Register event listener
 mobile.addEventListener( 'change', handleTabletChange)
 
 // Initial check
 handleTabletChange(mobile)
+
+// Modales
+const portfolioCards = Array.from(document.querySelectorAll('.portfolio-article-item'))
+const portfolioDescription = Array.from(document.querySelectorAll('.description'))
+
+function handlePortFolioCard(e){
+  e.stopPropagation()
+  console.log('this', this)
+  console.log('e.eventPhase', e.eventPhase)
+  console.log('e.target', e.target)
+}
+
+const handlePortfolioDescription = (e) => {
+}
+
+portfolioCards.forEach(item => {
+  item.addEventListener('click', handlePortFolioCard, true)
+})
+
+portfolioDescription.forEach(item => {
+  item.addEventListener('click', handlePortfolioDescription)
+})
