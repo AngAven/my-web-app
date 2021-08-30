5b4f21c8b2272057801c088d31d58c0d11a45d17
Angel <angel.aven.c@gmail.com>
2021-08-30 15:05:42 -0500

    Configuration array after request

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
 const mobile = window.matchMedia('(max-width: 600px)')
 const modal = document.querySelector('.btn')
 const closeModalButton = document.querySelector('.modal-close')
-const API = 'https://my.api.mockaroo.com/portfolio_data.json?key=d48cf750'
+const API = 'https://app.general.angelavendanocruz.com/general_api'
+// const API = 'https://my.api.mockaroo.com/portfolio_data.json?key=d48cf750'
 
 function handleTabletChange(e) {
   if (e.matches) {
@@ -21,47 +22,58 @@ const portfolioCards = Array.from(document.querySelectorAll('.portfolio-article-
 
 // Modal
 function addModalContent(cardData) {
-  const insertContentDate = document.querySelector('.insert-modal-content')
-  const image = cardData[0].image
-  const title = cardData[0].title
-  const sub_title = cardData[0].sub_title
-  const description = cardData[0].description
-  const technologies = cardData[0].technologies
-  const link = cardData[0].link
 
-  let figureFIGURE = document.createElement('figure')
-  let imageIMG = document.createElement('img')
-  let titleH2 = document.createElement('h2')
-  let subTitleH3 = document.createElement('h3')
-  let descriptionDIV = document.createElement('di')
-  let technologiesDIV = document.createElement('div')
-  let linkA = document.createElement('a')
-  let linkSMALL = document.createElement('small')
+  if (cardData.length > 0){
+    const insertContentDate = document.querySelector('.insert-modal-content')
+    const image = cardData[0].image
+    const title = cardData[0].title
+    const sub_title = cardData[0].sub_title
+    const description = cardData[0].description
+    const technologies = cardData[0].technologies
+    const link = cardData[0].link
 
-  imageIMG.src = image
-  titleH2.textContent = title
-  subTitleH3.textContent = sub_title
-  descriptionDIV.textContent = description
-  technologiesDIV.textContent = 'Technologies: ' + technologies
-  linkSMALL.textContent = 'Project Link'
-  linkA.href = link
+    let figureFIGURE = document.createElement('figure')
+    let imageIMG = document.createElement('img')
+    let titleH2 = document.createElement('h2')
+    let subTitleH3 = document.createElement('h3')
+    let descriptionDIV = document.createElement('di')
+    let technologiesDIV = document.createElement('div')
+    let linkA = document.createElement('a')
+    let linkSMALL = document.createElement('small')
 
-  figureFIGURE.appendChild(imageIMG)
-  figureFIGURE.classList.add('modal-image')
-  titleH2.classList.add('modal-title')
-  subTitleH3.classList.add('modal-subtitle')
-  descriptionDIV.classList.add('modal-description')
-  technologiesDIV.classList.add('modal-technologies')
-  linkA.classList.add('modal-link')
-  linkA.target = '_blank'
-  linkA.appendChild(linkSMALL)
+    imageIMG.src = image
+    titleH2.textContent = title
+    subTitleH3.textContent = sub_title
+    descriptionDIV.textContent = description
+    technologiesDIV.textContent = 'Technologies: ' + technologies
+    linkSMALL.textContent = 'Project Link'
+    linkA.href = link
 
-  insertContentDate.appendChild(figureFIGURE)
-  insertContentDate.appendChild(titleH2)
-  insertContentDate.appendChild(subTitleH3)
-  insertContentDate.appendChild(descriptionDIV)
-  insertContentDate.appendChild(technologiesDIV)
-  insertContentDate.appendChild(linkA)
+    figureFIGURE.appendChild(imageIMG)
+    figureFIGURE.classList.add('modal-image')
+    titleH2.classList.add('modal-title')
+    subTitleH3.classList.add('modal-subtitle')
+    descriptionDIV.classList.add('modal-description')
+    technologiesDIV.classList.add('modal-technologies')
+    linkA.classList.add('modal-link')
+    linkA.target = '_blank'
+    linkA.appendChild(linkSMALL)
+
+    insertContentDate.appendChild(figureFIGURE)
+    insertContentDate.appendChild(titleH2)
+    insertContentDate.appendChild(subTitleH3)
+    insertContentDate.appendChild(descriptionDIV)
+    insertContentDate.appendChild(technologiesDIV)
+    insertContentDate.appendChild(linkA)
+
+  } else{
+
+    const insertContentDate = document.querySelector('.insert-modal-content')
+    let titleH2 = document.createElement('h2')
+
+    titleH2.textContent = 'No data available'
+    insertContentDate.appendChild(titleH2)
+  }
 }
 
 async function getModalContent() {
@@ -72,10 +84,15 @@ async function getModalContent() {
 
 async function handlePortFolioCard(e){
   const itemNumber = Number(this.className.slice(-1))
-  const data = await getModalContent()
+  let data = await getModalContent()
 
-  const cardData = data.filter((el) => {
-    return el.id === itemNumber ? el.id : false
+  if (data.data){
+    data = data.data
+  }
+
+  const cardData = data.filter(el => {
+    const portfolio_id = Number(el.portfolio_id)
+    return portfolio_id === itemNumber ? el : false
   })
 
   addModalContent(cardData)
